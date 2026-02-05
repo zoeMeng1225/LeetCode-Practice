eae74d22b91022bce0227d92d412156a0e7621d4
zoeMeng1225 <46607720+zoeMeng1225@users.noreply.github.com>
2026-02-05 13:28:40 -0800

    Time: 44 ms (87.35%), Space: 54.9 MB (81.2%) - LeetHub

---
+++ 2627-debounce/2627-debounce.ts
@@ -0,0 +1,25 @@
+type F = (...args: number[]) => void
+
+function debounce(fn: F, t: number): F {
+    //store the timer ID in the closure so it persists across function calls
+    let timer:ReturnType<typeof setTimeout> | undefined;
+    return function(...args) {
+        //if a timer is already running(previous call is pending)
+        //reset the clock
+        if(timer){
+            clearTimeout(timer)
+        }
+        //set a new timer
+        //the function fn will only excute if this timer completes without being cleared;
+        timer = setTimeout(() => {
+            fn(...args)
+        },t)
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
