bd0ca5e30e80597f214692e5823414b4d48450ed
zoeMeng1225 <46607720+zoeMeng1225@users.noreply.github.com>
2026-02-11 10:37:18 -0800

    Time: 49 ms (33.99%), Space: 55.1 MB (47.45%) - LeetHub

--- 2621-sleep/2621-sleep.ts
+++ 2621-sleep/2621-sleep.ts
@@ -1,9 +1,5 @@
 async function sleep(millis: number): Promise<void> {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve()
-        }, millis)   
-    })
+   return new Promise(resolve => setTimeout(resolve, millis)) 
 }
 
 
