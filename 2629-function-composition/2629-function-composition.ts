0a8ab6eb81b6d510e099b62eb6903c5c81a0764c
zoeMeng1225 <46607720+zoeMeng1225@users.noreply.github.com>
2026-02-11 10:09:13 -0800

    Time: 56 ms (56.61%), Space: 56.6 MB (64.82%) - LeetHub

--- 2629-function-composition/2629-function-composition.ts
+++ 2629-function-composition/2629-function-composition.ts
@@ -1,22 +1,15 @@
 type F = (x: number) => number;
 
 function compose(functions: F[]): F {
- 
     return function(x) {
         let result = x;
-        if(functions.length === 0){
-            return x;
-        }
-
-        for(let i = functions.length -1; i >= 0; i--){
-            const fn = functions[i];
+        for(let i = functions.length -1 ; i >=0; i--){
+            const fn = functions[i]
             result = fn(result);
         }
 
-        return result
+        return result;
     }
-
-    
 };
 
 /**
