efc5188dc7ea934765d09543f4fd3c58223f1952
zoeMeng1225 <46607720+zoeMeng1225@users.noreply.github.com>
2026-02-13 09:48:42 -0800

    Time: 82 ms (6.06%), Space: 58.3 MB (10.91%) - LeetHub

---
+++ 2721-execute-asynchronous-functions-in-parallel/2721-execute-asynchronous-functions-in-parallel.ts
@@ -0,0 +1,30 @@
+type Fn<T> = () => Promise<T>
+
+function promiseAll<T>(functions: Fn<T>[]): Promise<T[]> {
+    return new Promise((resolve, reject) => {
+        const result:T[]= [];
+        let count = 0;
+
+        if(functions.length === 0){
+            resolve([]);
+            return;
+        }
+
+        functions.forEach((item, index) => {
+            Promise.resolve(item()).then(value => {
+                result[index] = value;
+                console.log('result:',result[index], 'value',value)
+                count++;
+
+                if(count === functions.length){
+                    resolve(result)
+                }
+            }).catch(err => reject(err))
+        })
+    })
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
