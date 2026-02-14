5046b0a9014fbefe67d2bd3f932f732ca34968f2
zoeMeng1225 <46607720+zoeMeng1225@users.noreply.github.com>
2026-02-14 09:57:31 -0800

    Time: 161 ms (66.06%), Space: 91 MB (21.1%) - LeetHub

---
+++ 2649-nested-array-generator/2649-nested-array-generator.ts
@@ -0,0 +1,20 @@
+type MultidimensionalArray = (MultidimensionalArray | number)[]
+
+function* inorderTraversal(arr: MultidimensionalArray): Generator<number, void, unknown> {
+   for(const item of arr){
+        if(Array.isArray(item)){
+            yield* inorderTraversal(item)
+        }else{
+            yield item;
+        }
+   }
+
+   
+};
+
+/**
+ * const gen = inorderTraversal([1, [2, 3]]);
+ * gen.next().value; // 1
+ * gen.next().value; // 2
+ * gen.next().value; // 3
+ */
