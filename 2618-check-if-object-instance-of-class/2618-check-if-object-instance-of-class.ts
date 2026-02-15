6ac79f640b4f755ddb33c73690e55f116229dd56
zoeMeng1225 <46607720+zoeMeng1225@users.noreply.github.com>
2026-02-15 13:28:47 -0800

    Time: 55 ms (97.89%), Space: 61.8 MB (98.73%) - LeetHub

--- 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.ts
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.ts
@@ -1,20 +1,31 @@
 function checkIfInstanceOf(obj: any, classFunction: any): boolean {
+    //input validation: return false, if 'obj' is null or undefined, or if 'classFunction' is not a valid function.
+    // Note: `obj === null` catches null, and `obj === undefined` catches undefined.
     if(obj === null || obj === undefined || typeof classFunction !== 'function'){
         return false;
     }
 
+    //get the target prototype(the DNA we are looking for)
+    //prototype property of the constructor function
     const targetPrototype = classFunction.prototype;
-    
+
+    //get the initial prototype of the object;
+    //use 'Object.getPrototypeOf()' instead of accessing '__proto__' diractly
+    //key benifit: this method support primitive types(like 5, 'string') by automatically 'boxing' them whereas 'instanceof' would fail on primitives.
     let currentProto = Object.getPrototypeOf(obj);
     
+    //traverse up the prototype chain
     while(currentProto !== null){
+        //check if the current prototype matches the target
         if(currentProto === targetPrototype){
             return true;
         }
-
+        
+        //if not found, move one level up the prototype chain
         currentProto = Object.getPrototypeOf(currentProto)
     }
 
+    //reached the end of the chain without finding a match
     return false
 };
 
