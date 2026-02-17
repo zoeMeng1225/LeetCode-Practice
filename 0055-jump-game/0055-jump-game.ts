cd399039e9825d86d7f0af917b23e6863a4fa57d
zoeMeng1225 <46607720+zoeMeng1225@users.noreply.github.com>
2026-02-17 13:47:18 -0800

    Time: 1 ms (84.46%), Space: 59.6 MB (71.47%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.ts
@@ -0,0 +1,16 @@
+function canJump(nums: number[]): boolean {
+    //logic，maintain a variable(fastest place can reach)
+    let maxReach = 0;
+    const target = nums.length - 1;
+
+    for(let i = 0; i < nums.length; i++){
+        //if current i has exceeded max range, it means that it has been broken and can't be jumped over;
+        if(i > maxReach) return false;
+
+        //current position i(See if it was far before or far now)
+        maxReach = Math.max(maxReach, i + nums[i]);
+
+        if(maxReach >= target) return true;
+    }
+    return true
+};
