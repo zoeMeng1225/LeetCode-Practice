fdcb78ea162253aaa8fab17b9659f1f4a5df51d7
zoeMeng1225 <46607720+zoeMeng1225@users.noreply.github.com>
2026-02-24 09:49:48 -0800

    Time: 1 ms (77.05%), Space: 55.3 MB (76.59%) - LeetHub

--- 0274-h-index/0274-h-index.ts
+++ 0274-h-index/0274-h-index.ts
@@ -1,16 +1,20 @@
 function hIndex(citations: number[]): number {
-    //sort the citations array indescending order
-    //for check the paper with highest citation
-    citations.sort((a, b) => b - a);
+    //sort the citations array in descending order;
+    //this allow us to easily check the papers with the highest citations first
+    citations.sort((a, b) => b - a); //[3,0,6,1,5] -> [6, 5, 3, 1, 0]
 
     let h = 0;
 
-    //iterate through the sort array
-    //citation[h]: the citation count of the (h+ 1)th paper;
+    //iterate through the sorted array
+    //we check if the current paper has more citations than our current h count
+    //citation[h] represent the citation count of the (h + 1) -th paper
+    //e.g. citation[0](6) > 0, citation[1](5) > 1 ...
+    //h is play 2 roles: 1. index, it represent which article we are about read; 2. It is the "H-Index compliance quantity (Count)" that we have currently confirmed.
     while(h < citations.length && citations[h] > h){
         h++;
-        //if the paper has enough citation, increase our h-index
     }
 
+    //this loop breaks when a peper doesn't meet the condition
+    //at this point, h repersent s the maximum possible h-index;
     return h
 };
