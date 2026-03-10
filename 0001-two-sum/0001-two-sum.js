11d3453925e5f61f9fb7fe20c9d0378bcce30769
zoeMeng1225 <46607720+zoeMeng1225@users.noreply.github.com>
2026-03-09 20:40:48 -0700

    Time: 1 ms (87.38%), Space: 56.7 MB (13.54%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map();
+
+    for(let i = 0; i < nums.length; i++){
+        let num = nums[i]
+        let diff = target - nums[i];
+        if(map.has(diff)){
+            return [map.get(diff), i ]
+        }
+
+        map.set(num, i )
+    }
+
+    return []
+};
