f38fd63116d34e3aa8283b80c06e76f9a4bdee3e
zoeMeng1225 <46607720+zoeMeng1225@users.noreply.github.com>
2026-06-01 10:23:46 -0700

    Time: 566 ms (31.58%), Space: 0B (100%) - LeetHub

---
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+-- Select the product_id column 
+SELECT product_id
+
+-- from the products table
+FROM Products
+
+-- filter condition: low fat is Y AND recyclable is Y
+WHERE low_fats = "Y" AND recyclable = "Y"
