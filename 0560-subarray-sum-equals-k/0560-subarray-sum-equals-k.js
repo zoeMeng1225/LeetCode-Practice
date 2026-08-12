7617e627a55e18454f57a3915633dbed5a5710a2
zoeMeng1225 <46607720+zoeMeng1225@users.noreply.github.com>
2026-08-12 16:47:29 -0700

    Time: 16 ms (75.57%), Space: 65.4 MB (23.83%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -4,28 +4,20 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-    //use map to store the key-value pair like this:
-    //nums = [1,2,3], k = 3, the map should look like this
-    //[3, 1+2], [3, 3], so the output should be 2, becuase there are totle 2 nums that added to 3.
-
-    //we need find the sum of contiguos numbers is k. 
-    //store total number of valid subarrays
-    let count = 0;
-    let prefixSum = 0;
+    let count = 0, prefixSum = 0;
     const map = new Map();
-    //initialize: prefix sum 0 appear 1 time by defualt
-    map.set(0, 1)
+    map.set(0, 1);
+
     for(const num of nums){
-        //add the current number
         prefixSum += num;
-        const target = prefixSum - k 
+        const target = prefixSum - k
         if(map.has(target)){
-            count+=map.get(target)
+            count += map.get(target)
         }
-        
+
         const freq = map.get(prefixSum) || 0;
-        map.set(prefixSum, freq +1) // added current time
+        map.set(prefixSum, freq + 1);
     }
 
-    return count
+    return count;
 };
