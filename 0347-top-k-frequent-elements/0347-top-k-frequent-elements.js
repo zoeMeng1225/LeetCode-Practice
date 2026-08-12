67d4ad5677db1cd77d7535a95f49bb45bc3bfb8f
zoeMeng1225 <46607720+zoeMeng1225@users.noreply.github.com>
2026-08-12 15:18:43 -0700

    Time: 13 ms (54.91%), Space: 58.6 MB (81.73%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -3,19 +3,16 @@
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function (nums, k) {
-  let map = new Map();
-  for (const num of nums) {
-    const currentCount = map.get(num) || 0;
-    map.set(num, currentCount + 1);
-  }
+var topKFrequent = function(nums, k) {
+    let map = new Map();
 
-  //convert the map the 2d array
-  const entires = Array.from(map.entries());
-  const result = entires
-    .sort((a, b) => b[1] - a[1])
-    .slice(0, k)
-    .map((e) => e[0]);
+    for(const num of nums){
+        const group = map.get(num) || 0;
+        map.set(num, group + 1);
+    }
 
-  return result;
+    const entries = Array.from(map.entries())
+    const result = entries.sort((a, b) => b[1] - a[1]).slice(0, k).map(e => e[0])
+    console.log(result)
+    return result
 };
