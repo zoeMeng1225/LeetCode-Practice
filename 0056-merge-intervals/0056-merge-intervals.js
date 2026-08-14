ad63843b7f336ca50c7302178f58302aa2d8f362
zoeMeng1225 <46607720+zoeMeng1225@users.noreply.github.com>
2026-08-14 09:04:45 -0700

    Time: 5 ms (96.5%), Space: 63.9 MB (85.25%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -3,23 +3,26 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    
-    intervals.sort((a, b) => a[0] - b[0]);
-    
-    const result = [];
-    let temp = intervals[0];
-    
+    if(intervals.length === 0) return [];
+
+    intervals.sort((a,b)=> a[0] - b[0])
+
+    const result = [intervals[0]]
+
+
+
     for(let i = 1; i < intervals.length; i++){
-        const item = intervals[i];
-        if(temp[1] >= item[0]){
-            temp[1] = Math.max(temp[1], item[1]);
+        const currentInterval = intervals[i];
+        const lastMergedInterval = result[result.length -1];
+
+        //check if it overlap, current starting < last merger interval
+        if(currentInterval[0] <= lastMergedInterval[1]){
+            lastMergedInterval[1] = Math.max(lastMergedInterval[1], currentInterval[1])
         }else{
-            result.push(temp);
-            temp = item;
-            
+            //if not overlap
+            result.push(currentInterval)
         }
     }
-    
-    result.push(temp);
-    return result;
+
+    return result
 };
